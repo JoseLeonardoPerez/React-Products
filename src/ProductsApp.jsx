f0b63a0292af440ebb1d9fbe50b119cfcc1fee71
JoseLeonardoPerez <joseleonardoperez4@gmail.com>
2025-09-05 18:51:15 -0300

    Actualizado para trabajar con backend

--- src/ProductsApp.jsx
+++ src/ProductsApp.jsx
@@ -2,19 +2,22 @@ import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { ProductTable } from './components/ProductTable';
 import { ProductForm } from './components/ProductForm';
+import { create, findAll, remove, update } from './services/productService';
+import Swal from 'sweetalert2';
 
-const initProducts = [{
-    id:1,
-    name: 'Monitor Asus 37 pulkgadas',
-    description: 'El monitor es perfecto para Juegos!',
-    price: 1000
-}, 
-{
-    id:2,
-    name: 'Iphone 16 pro',
-    description: 'El telefono es excelente e incluye Apple Intelligence!',
-    price: 1600
-}];
+
+// const initProducts = [{
+//     id:1,
+//     name: 'Monitor Asus 37 pulkgadas',
+//     description: 'El monitor es perfecto para Juegos!',
+//     price: 1000
+// }, 
+// {
+//     id:2,
+//     name: 'Iphone 16 pro',
+//     description: 'El telefono es excelente e incluye Apple Intelligence!',
+//     price: 1600
+// }];
 
 export const ProductsApp = ({ title = 'Title default!'}) => {
 
@@ -26,38 +29,76 @@ export const ProductsApp = ({ title = 'Title default!'}) => {
         price: ''
     });
 
-
+    const getProducts = async () => {
+      const result = await findAll();
+      setProducts(result.data);
+    }
     useEffect(()  => {
-      setProducts(initProducts);
+      getProducts();
       console.log('cargando la pagina ...')
     }, []);
 
 
-    const handlerAddProduct = (product) => {
+    const handlerAddProduct = async(product) => {
       if (product.id > 0){
+        const response = await update(product);
         setProducts(
            products.map(prod =>{
              if(prod.id == product.id){
-              return {...product};
+              return {...response.data};
              }
              return prod;
            })
         );
+       Swal.fire({
+          title: "Actualizado con exito!",
+          text: `Producto ${product.name} actualizado con exito!`,
+          icon: "success"
+        })  
+
       }else {
-        setProducts([...products, {...product, id: Date.now()}]);
+        const response = await create(product);
+        setProducts([...products, {...response.data}]);
+        Swal.fire({
+          title: "Creado con exito!",
+          text: `Producto ${product.name} creado con exito!`,
+          icon: "success"
+        })
       }
       
     }
 
     const handlerProductSelected = (product) => {
        setProductSelected({...product})
-       console.log(productSelected);
+       
     }
 
     const handlerRemoveProduct = (id) => {
-       setProducts(
+
+      Swal.fire({
+       title: "Esta seguro que quiere eliminar?",
+       text: "Cuidado, accion eliminara producto del sistema!",
+       icon: "Advertencia",
+       showCancelButton: true,
+       confirmButtonColor: "#3085d6",
+       cancelButtonColor: "#d33",
+       confirmButtonText: "Si, eliminar!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+            remove(id);
+            setProducts(
           products.filter( product => product.id!= id)
-       )
+       );
+       Swal.fire({
+          title: "Eliminado con exito!",
+          text: `Producto eliminado con exito!`,
+          icon: "success"
+        });
+  }
+});
+
+
+
     }
 
     return (<div className='container my-4'>
